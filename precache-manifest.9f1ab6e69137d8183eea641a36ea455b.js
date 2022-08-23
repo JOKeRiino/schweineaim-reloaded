8921b80c2e14663b0fdf0797c7f034ac75f35bed
JOKeRiino <dev@simontroester.com>
2022-08-23 10:28:23 +0200

    Updates

---
+++ precache-manifest.9f1ab6e69137d8183eea641a36ea455b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "71a1ee8fdd9ad7331bf5",
+    "url": "/static/css/main.bb1492b1.chunk.css"
+  },
+  {
+    "revision": "71a1ee8fdd9ad7331bf5",
+    "url": "/static/js/main.71a1ee8f.chunk.js"
+  },
+  {
+    "revision": "1d01e418c44d52eb159b",
+    "url": "/static/js/1.1d01e418.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b651e29e2ab1b84d54a4449a3663e783",
+    "url": "/static/media/trilluxe.b651e29e.webp"
+  },
+  {
+    "revision": "3f8693fb7f292182d9b004c8c241e707",
+    "url": "/static/media/röder.3f8693fb.webp"
+  },
+  {
+    "revision": "3e1de6c0201cc4921b621f1f1145e0d2",
+    "url": "/static/media/marf.3e1de6c0.webp"
+  },
+  {
+    "revision": "1e5bceef2a07a4e05a3979b0be6af6fd",
+    "url": "/static/media/fibu.1e5bceef.webp"
+  },
+  {
+    "revision": "5080c96494841eb88d3dfb7d1212d0fb",
+    "url": "/static/media/blader.5080c964.webp"
+  },
+  {
+    "revision": "0c1025cadc295ac4d3044791f7886d56",
+    "url": "/static/media/nallik.0c1025ca.webp"
+  },
+  {
+    "revision": "edff82dd06273941189fbe5ea056f82d",
+    "url": "/static/media/smith.edff82dd.webp"
+  },
+  {
+    "revision": "6f479a8ee6f0fab38cc8cdafea38f696",
+    "url": "/static/media/logo.6f479a8e.webp"
+  },
+  {
+    "revision": "9f9bf257fbe71ce5442575bc2b96d892",
+    "url": "/static/media/riino.9f9bf257.webp"
+  },
+  {
+    "revision": "197474486c89b8a022ea463288f9900b",
+    "url": "/static/media/modcheck.19747448.gif"
+  },
+  {
+    "revision": "8372c513ea8c346a21789216de3e232e",
+    "url": "/index.html"
+  }
+];
